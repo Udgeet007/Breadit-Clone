d7159984ae15cbcde98e2e5763b2e3f3622d6db9
Udgeet <bhatt8udgeet@gmail.com>
2024-01-05 14:30:05 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import CustomFeed from '@/components/homepage/CustomFeed'
-import GeneralFeed from '@/components/homepage/GeneralFeed'
 import { buttonVariants } from '@/components/ui/Button'
 import { getAuthSession } from '@/lib/auth'
 import { Home as HomeIcon } from 'lucide-react'
@@ -15,8 +13,7 @@ export default async function Home() {
     <>
       <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
-        {/* @ts-expect-error server component */}
-        {session ? <CustomFeed /> : <GeneralFeed />}
+        {/* feed */}
 
         {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
@@ -26,7 +23,7 @@ export default async function Home() {
               Home
             </p>
           </div>
-          <dl className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
+          <div className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
             <div className='flex justify-between gap-x-4 py-3'>
               <p className='text-zinc-500'>
                 Your personal Breadit frontpage. Come here to check in with your
@@ -41,7 +38,7 @@ export default async function Home() {
               href={`/r/create`}>
               Create Community
             </Link>
-          </dl>
+          </div>
         </div>
       </div>
     </>
--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -47,7 +47,16 @@ const Page: FC = () => {
           return loginToast();
         }
       }
+
+      toast({
+        title: "There was an error.",
+        description: "Could not create subreddit.",
+        variant: "destructive",
+      })
     },
+    onSuccess: (data) => {
+      router.push(`/r/${data}`)
+    }, 
   });
 
   return (
