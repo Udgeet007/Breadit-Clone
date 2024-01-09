ee24d3fb32eab3a74879f5eb6e7582a810f8d1ef
Udgeet <bhatt8udgeet@gmail.com>
2024-01-09 14:10:35 +0530

    Created Subreddit  Detail Page.

---
+++ src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface SubscribeLeaveToggleProps {
+  
+}
+
+const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({}) => {
+
+ return (
+  <div> h</div>
+ )
+
+}
+
+export default SubscribeLeaveToggle
